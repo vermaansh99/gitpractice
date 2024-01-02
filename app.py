98d67b42c6bff474ae4a75c8a5713b532674bfd3
Ansh verma <anshverma2073@gmail.com>
2024-01-03 00:41:22 +0530

    code changes added

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ def addition(a,b,c):
     return a+b+c
 
 
-def substraction(a,b):
-    return a-b
+def substraction(a,b,c):
+    return a-b-c
 
 
-def divison(a,b):
-    return a/b
+def divison(a,b,c):
+    return a/b/c
 
 
 class BankAccount:
