bdf451aff528cd99a405e961e639a4a9b1d3b5a0
Ansh verma <anshverma2073@gmail.com>
2024-01-03 00:28:57 +0530

    delevlopera code

--- app.py
+++ app.py
@@ -4,3 +4,31 @@ def addition(a,b):
 
 def substraction(a,b):
     return a-b
+
+
+def divison(a,b):
+    return a/b
+
+
+class BankAccount:
+    def __init__(self):
+        self.balance = 0
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.balance:
+            self.balance -= amount
+        else:
+            print("Insufficient funds!")
+
+    def get_balance(self):
+        return self.balance
+
+
+account = BankAccount()
+account.deposit(100)
+account.withdraw(30)
+print(account.get_balance())  # Output: 70
